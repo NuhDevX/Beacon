3e9ef32aeabfb633bd885b41f2cc551fb0ca36d4
NuhDev <92098247+NuhDevX@users.noreply.github.com>
2024-10-21 11:04:25 +0700

    Update Loader.php

--- src/muqsit/beacon/Loader.php
+++ src/muqsit/beacon/Loader.php
@@ -4,18 +4,16 @@ declare(strict_types=1);
 
 namespace muqsit\beacon;
 
-use muqsit\beacon\block\BlockFactory;
 use muqsit\beacon\timings\BeaconTimings;
+use pocketmine\block\tile\TileFactory;
 use pocketmine\plugin\PluginBase;
+use muqsit\beacon\block\tile\Beacon as BeaconTile;
 
 final class Loader extends PluginBase{
-
-	protected function onLoad() : void{
-		BlockFactory::init();
-	}
 
 	protected function onEnable() : void{
 		BeaconTimings::init($this);
 		new BeaconInventoryNetworkListener($this);
+		TileFactory::getInstance()->register(BeaconTile::class, ["evdbeacon:beacon"]);
 	}
 }
